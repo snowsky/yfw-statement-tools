3c70df38048b60577ba91d50995e3594d8e961a4
Hao Wang <hao.1.wang@gmail.com>
2026-04-10 12:26:26 -0400

    Fix Invalid API key

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import UploadPage from './pages/UploadPage'
 import PublicDownloadPage from './pages/PublicDownloadPage'
